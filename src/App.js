3b344d77ed8eb87a86626036c9f7d549419b65ac
muritadb <douglasb008@gmail.com>
2021-07-06 22:24:39 -0300

    "refatoração nos componentes e estudo de API "

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { FaSearch } from 'react-icons/fa';
 import Capitais from './UI/Capitais';
-import Pesquisa from './UI/Pesquisa';
+import Modal from './UI/Modal';
+import axios from 'axios';
+
+
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false); //estado para fechar janela
+  const [searchCity, setSearchCity] = useState(''); //estado que busca a cidade no input
+
+  function handlePrevisao() {
+    setIsOpen(true);
+    axios.get(`https://api.hgbrasil.com/weather?key=74da3f73&city_name= ${searchCity}`).then(response => console.log(response))
+      .catch(e => { console.log(e) });
+    //console.log(searchCity)
+
+  }
+
+
+
 
   return (
     <div className="container">
       <h1>Previsão do Tempo</h1>
+      <Modal open={isOpen} onClose={() => { setIsOpen(false) }} />
 
-      <Pesquisa />
+      <section className="container-search">
+        <input
+          type="text"
+          placeholder="Qual a sua Cidade"
+          value={searchCity}
+          onChange={e => setSearchCity(e.target.value)}
+
+        />
+        <button className="container-search_btn" onClick={handlePrevisao} >
+          <FaSearch />
+        </button>
+      </section>
       <Capitais />
     </div>
   );
